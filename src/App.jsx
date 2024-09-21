dc4d5dc17fa85438fb347ed72d8a8ff99e5681d9
ghv061101 <ghv061101@gmail.com>
2024-09-21 14:11:05 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'  
+import Collection from './pages/Collection'
+import Cart from './pages/Cart'  
+import PlaceOrder from './pages/PlaceOrder'  
+import Orders from './pages/Orders'  
+import Chatbot from './components/Chatbot'  
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collections' element={<Collection />} />  
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
+      </Routes>
+      <Chatbot />
+    </div>
   )
 }
 
